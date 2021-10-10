4d5b7f9c2695aa01495fe68d60866a6d3b940843
Vladislav <dudashvilivladislav@mail.ru>
2021-10-10 14:46:50 +0300

    cm

--- src/api/requests/orders.js
+++ src/api/requests/orders.js
@@ -1,7 +1,20 @@
-import axios from '../instance'
+import axios from "../instance";
 
 export default {
-    sendOrder(data) {
+  /*     sendOrder(data) {
         return axios.post("/order/sendOrder", data)
-    }
-}
+    } */
+
+  sendOrder: {
+    get() {
+      return axios.get("/order/sendOrder");
+    },
+    create(client_id, table_json) {
+      return axios.post(
+        `/order/sendOrder?client_id=${client_id}&table_json=${JSON.stringify(
+          table_json
+        )}`
+      );
+    },
+  },
+};
